9a69c4c5282711d555699d453ab50bae7e91f7e4
Ruslan <ruslanfilistovich@gmail.com>
2019-08-13 17:17:31 +0300

    first commit

---
+++ ArtCover2.0/ArtCoverCell.swift
@@ -0,0 +1,14 @@
+//
+//  ArtCoverCell.swift
+//  ArtCover2.0
+//
+//  Created by Ruslan on 8/13/19.
+//  Copyright © 2019 Ruslan Filistovich. All rights reserved.
+//
+
+import UIKit
+
+class ArtCoverCell: UICollectionViewCell {
+    
+    @IBOutlet weak var coverImageView: UIImageView!
+}
